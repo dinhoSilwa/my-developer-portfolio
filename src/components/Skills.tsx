e95327a9c4a9c12ec779f028383c1ef5756d3098
claudio silva <claudiosilva.one@gmail.com>
2024-12-29 13:26:05 -0300

    feat: add-responsiveness-to-skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,10 +2,14 @@ import { svgIcons } from "../assets/skills";
 
 export const Skills = () => {
   return (
-    <section className="flex flex-col justify-center gap-8 items-center ">
-      <h2 className="text-extra font-bold text-white">Minhas Habilidades</h2>
-
-      <div className="container-skill lg:w-[70%] flex p-8 flex-wrap justify-center items-center gap-12 lg:px-18 lg:h-[400px] xl:px-20">
+    <section className="mb-32 flex flex-col justify-center gap-8 items-center ">
+      <header className="text-center flex gap-1 flex-col">
+        <h2 className="text-extra font-bold text-white">Minhas Habilidades</h2>
+        <p className="text-light-primary/90 mobile-small:px-6">
+          Tecnologias e linguagens que uso no meu dia a dia profissional
+        </p>
+      </header>
+      <div className="container-skill lg:w-[968px]  flex p-8 flex-wrap justify-center items-center gap-12 lg:px-18 lg:h-[400px] xl:px-20">
         {svgIcons.map(({ name, icon: Icon }, index) => (
           <div
             key={index}
