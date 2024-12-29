fc0f1fda7d7f48465040729c6809b2a6fdb6f4a2
claudio silva <claudiosilva.one@gmail.com>
2024-12-28 22:19:31 -0300

    chore: add-and-configure-tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'green': '0 4px 100px 10px rgba(7, 121, 49, 0.5), 0 2px 4px -1px rgba(7, 121, 49, 0.3)',
+        "text-green": '0 0 10px rgba(0, 0, 0, 0.5)'
+      },
+      fontSize: {
+        "mini-primary": "6px",
+        "mini": "8px",
+        "small": "10px",
+        "medium": "12px",
+        "medium-second": "14px",
+        "larger": "18px",
+        "larger-second": "20px",
+        "extra": "32px",
+        "extra-second": "40px"
+      },
+      fontFamily: {
+        "primary": "DM Sans",
+        "second": "Poppins"
+      },
+      colors: {
+        "dark-primary": '#191919',
+        "light-primary": '#A7A7A7'
+      }
+    },
+  },
+  plugins: [],
+}
+
